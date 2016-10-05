64a09ff2d7a01c07ded7fad491cc4e1631387081
Jiří Otáhal <xotahal@gmail.com>
2016-10-05 14:27:09 +0200

    Add search toolbar
    Just to be sure it works properly

--- src/App/Home.react.js
+++ src/App/Home.react.js
@@ -5,7 +5,7 @@ import routes from '../routes';
 
 import Container from '../Container';
 // components
-import { ActionButton, ListItem, Toolbar } from '../react-native-material-ui/src';
+import { ActionButton, Avatar, ListItem, Toolbar, COLOR } from '../react-native-material-ui/src';
 
 const propTypes = {
     navigator: PropTypes.object.isRequired,
@@ -13,77 +13,99 @@ const propTypes = {
 };
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selected: [],
+            searchText: '',
+        };
+    }
+    onAvatarPressed = (value) => {
+        const { selected } = this.state;
+
+        const index = selected.indexOf(value);
+
+        if (index >= 0) {
+            // remove item
+            selected.splice(index, 1);
+        } else {
+            // add item
+            selected.push(value);
+        }
+
+        this.setState({ selected });
+    }
+    renderToolbar = () => {
+        if (this.state.selected.length > 0) {
+            return (
+                <Toolbar
+                    key="toolbar"
+                    leftElement="clear"
+                    onLeftElementPress={() => this.setState({ selected: [] })}
+                    centerElement={this.state.selected.length.toString()}
+                    rightElement={['delete']}
+                    style={{
+                        container: { backgroundColor: 'white' },
+                        titleText: { color: 'rgba(0,0,0,.87)' },
+                        leftElement: { color: 'rgba(0,0,0,.54)' },
+                        rightElement: { color: 'rgba(0,0,0,.54)' },
+                    }}
+                />
+            );
+        }
+        return (
+            <Toolbar
+                key="toolbar"
+                leftElement="menu"
+                onLeftElementPress={() => this.props.navigator.pop()}
+                centerElement={this.props.route.title}
+                searchable={{
+                    autoFocus: true,
+                    placeholder: 'Search',
+                    onChangeText: value => this.setState({ searchText: value }),
+                    onSearchClosed: () => this.setState({ searchText: '' }),
+                }}
+            />
+        );
+    }
+    renderItem = (title, route) => {
+        const searchText = this.state.searchText.toLowerCase();
+
+        if (searchText.length > 0 && title.toLowerCase().indexOf(searchText) < 0) {
+            return null;
+        }
+
+        return (
+            <ListItem
+                divider
+                leftElement={<Avatar text={title[0]} />}
+                onLeftElementPress={() => this.onAvatarPressed(title)}
+                centerElement={title}
+                onPress={() => this.props.navigator.push(route)}
+            />
+
+        );
+    }
     render() {
         return (
             <Container>
-                <Toolbar
-                    leftElement="arrow-back"
-                    onLeftElementPress={() => this.props.navigator.pop()}
-                    centerElement={this.props.route.title}
-                    searchable={{
-                        placeholder: 'Search',
-                        autoFocus: true,
-                    }}
-                />
+                {this.renderToolbar()}
                 <ScrollView
                     keyboardShouldPersistTaps
                     keyboardDismissMode="interactive"
                 >
-                    <ListItem
-                        divider
-                        centerElement="Action buttons"
-                        onPress={() => this.props.navigator.push(routes.actionButton)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Avatars"
-                        onPress={() => this.props.navigator.push(routes.avatar)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Buttons"
-                        onPress={() => this.props.navigator.push(routes.button)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Cards"
-                        onPress={() => this.props.navigator.push(routes.card)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Checkbox"
-                        onPress={() => this.props.navigator.push(routes.checkbox)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Dialog"
-                        onPress={() => this.props.navigator.push(routes.dialog)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Drawer"
-                        onPress={() => this.props.navigator.push(routes.drawer)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Icon toggles"
-                        onPress={() => this.props.navigator.push(routes.iconToggle)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="List items"
-                        onPress={() => this.props.navigator.push(routes.list)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Radio buttons"
-                        onPress={() => this.props.navigator.push(routes.radioButton)}
-                    />
-                    <ListItem
-                        divider
-                        centerElement="Toolbars"
-                        onPress={() => this.props.navigator.push(routes.toolbar)}
-                    />
+                    {this.renderItem('Action buttons', routes.actionButton)}
+                    {this.renderItem('Avatars', routes.avatar)}
+                    {this.renderItem('Buttons', routes.button)}
+                    {this.renderItem('Cards', routes.card)}
+                    {this.renderItem('Checkbox', routes.checkbox)}
+                    {this.renderItem('Dialog', routes.dialog)}
+                    {this.renderItem('Drawer', routes.drawer)}
+                    {this.renderItem('Icon toggles', routes.iconToggle)}
+                    {this.renderItem('List items', routes.list)}
+                    {this.renderItem('Radio buttons', routes.radioButton)}
+                    {this.renderItem('Toolbars', routes.toolbar)}
                 </ScrollView>
                 <ActionButton
                     actions={['email', 'phone', 'sms', 'favorite']}
