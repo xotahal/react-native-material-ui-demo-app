6c545d1a4525d877e466b33f69725aab8c1284ca
Jiří Otáhal <xotahal@gmail.com>
2017-01-10 15:51:37 +0100

    Animate action button when scroll

--- src/App/Home.react.js
+++ src/App/Home.react.js
@@ -1,11 +1,20 @@
 import React, { Component, PropTypes } from 'react';
-import { ToastAndroid, ScrollView, Platform } from 'react-native';
+import { ToastAndroid, ScrollView, Platform, Animated, Easing } from 'react-native';
 
 import routes from '../routes';
 
 import Container from '../Container';
 // components
-import { ActionButton, Avatar, ListItem, Toolbar } from '../react-native-material-ui/src';
+import {
+    ActionButton,
+    Avatar,
+    ListItem,
+    Toolbar,
+    BottomNavigation,
+} from '../react-native-material-ui/src';
+
+const UP = 1;
+const DOWN = -1;
 
 const propTypes = {
     navigator: PropTypes.object.isRequired,
@@ -16,9 +25,15 @@ class Home extends Component {
     constructor(props) {
         super(props);
 
+        this.offset = 0;
+        this.scrollDirection = 0;
+
         this.state = {
             selected: [],
             searchText: '',
+            toolbarHidden: false,
+            active: 'people',
+            moveAnimated: new Animated.Value(0),
         };
     }
     onAvatarPressed = (value) => {
@@ -35,6 +50,44 @@ class Home extends Component {
         }
 
         this.setState({ selected });
+    }
+    onScroll = (ev) => {
+        const currentOffset = ev.nativeEvent.contentOffset.y;
+
+        const sub = this.offset - currentOffset;
+
+        // don't care about very small moves
+        if (sub > -2 && sub < 2) {
+            return;
+        }
+
+        this.offset = ev.nativeEvent.contentOffset.y;
+
+        const currentDirection = sub > 0 ? UP : DOWN;
+
+        if (this.scrollDirection !== currentDirection) {
+            this.scrollDirection = currentDirection;
+
+            this.setState({
+                bottomHidden: currentDirection === DOWN,
+            });
+        }
+    }
+    show = () => {
+        Animated.timing(this.state.moveAnimated, {
+            toValue: 0,
+            duration: 225,
+            easing: Easing.bezier(0.0, 0.0, 0.2, 1),
+            useNativeDriver: Platform.OS === 'android',
+        }).start();
+    }
+    hide = () => {
+        Animated.timing(this.state.moveAnimated, {
+            toValue: 56, // because the bottom navigation bar has height set to 56
+            duration: 195,
+            easing: Easing.bezier(0.4, 0.0, 0.6, 1),
+            useNativeDriver: Platform.OS === 'android',
+        }).start();
     }
     renderToolbar = () => {
         if (this.state.selected.length > 0) {
@@ -94,6 +147,7 @@ class Home extends Component {
                 <ScrollView
                     keyboardShouldPersistTaps
                     keyboardDismissMode="interactive"
+                    onScroll={this.onScroll}
                 >
                     {this.renderItem('Action buttons', routes.actionButton)}
                     {this.renderItem('Avatars', routes.avatar)}
@@ -116,6 +170,7 @@ class Home extends Component {
                         { icon: 'sms', label: 'Text' },
                         { icon: 'favorite', label: 'Favorite' },
                     ]}
+                    hidden={this.state.bottomHidden}
                     icon="share"
                     transition="speedDial"
                     onPress={(action) => {
@@ -123,7 +178,40 @@ class Home extends Component {
                             ToastAndroid.show(action, ToastAndroid.SHORT);
                         }
                     }}
+                    style={{
+                        positionContainer: { bottom: 76 },
+                    }}
                 />
+                <BottomNavigation
+                    active={this.state.active}
+                    hidden={this.state.bottomHidden}
+                    style={{ container: { position: 'absolute', bottom: 0, left: 0, right: 0 } }}
+                >
+                    <BottomNavigation.Action
+                        key="today"
+                        icon="today"
+                        label="Today"
+                        onPress={() => this.setState({ active: 'today' })}
+                    />
+                    <BottomNavigation.Action
+                        key="people"
+                        icon="people"
+                        label="People"
+                        onPress={() => this.setState({ active: 'people' })}
+                    />
+                    <BottomNavigation.Action
+                        key="bookmark-border"
+                        icon="bookmark-border"
+                        label="Bookmark"
+                        onPress={() => this.setState({ active: 'bookmark-border' })}
+                    />
+                    <BottomNavigation.Action
+                        key="settings"
+                        icon="settings"
+                        label="Settings"
+                        onPress={() => this.setState({ active: 'settings' })}
+                    />
+                </BottomNavigation>
             </Container>
 
 
