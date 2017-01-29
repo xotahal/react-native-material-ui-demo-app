4336591ba6629d3cdd19f4817163cb3be2fb99b9
Jiří Otáhal <xotahal@gmail.com>
2017-01-29 15:58:01 +0100

    Add three lines list item example

--- src/List/index.js
+++ src/List/index.js
@@ -13,9 +13,15 @@ const propTypes = {
     navigator: PropTypes.object.isRequired,
     route: PropTypes.object.isRequired,
 };
+const contextTypes = {
+    uiTheme: PropTypes.object.isRequired,
+};
 
 class List extends Component {
     render() {
+        const { listItem } = this.context.uiTheme;
+        const flattenPrimaryText = StyleSheet.flatten(listItem.primaryText);
+
         return (
             <View style={styles.container}>
                 <Toolbar
@@ -112,6 +118,27 @@ class List extends Component {
                         }}
                         numberOfLines={3}
                     />
+                    <ListItem
+                        divider
+                        leftElement="person"
+                        centerElement={{
+                            primaryText: 'Center element as an object',
+                            secondaryText: 'Pellentesque commodo ultrices diam. Praesent in ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing.',
+                            tertiaryText: 'Praesent in ipsum. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet',
+                        }}
+                    />
+                    <ListItem
+                        divider
+                        leftElement="person"
+                        centerElement={{
+                            primaryText: 'Ali Connors',
+                            secondaryText: 'Brunch this weekend?',
+                            tertiaryText: 'Pellentesque commodo ultrices diam. Praesent in ipsum.',
+                        }}
+                        style={{
+                            secondaryText: { color: flattenPrimaryText.color },
+                        }}
+                    />
                     <Subheader text="Dynamic" />
                     <ListItem
                         divider
@@ -136,5 +163,6 @@ class List extends Component {
 }
 
 List.propTypes = propTypes;
+List.contextTypes = contextTypes;
 
 export default List;
